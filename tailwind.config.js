a9232c6cf78a22063ce9fe1f1d1773644149c40f
bykriz <cristianesquivelh@hotmail.com>
2023-04-10 18:15:32 -0400

    agregando colores personalizados

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      // Primary Colors
+      "slider-full-color": "#a5f3eb",
+      "slider-bg": "#10d5c2",
+      "discount-bg": "#feece7",
+      "discount-text": "#ff8c66",
+      "cta-text": "#bdccff",
+
+      // Neutral Colors
+      "pricing-component-bg": "#ffffff",
+      "main-bg": "#fafbff",
+      "slider-empty-color": "#eaeefb",
+      "toggle-bg": "#cdd7ee",
+      "text-color": "#858fad",
+      "cta-bg": "#293356",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
