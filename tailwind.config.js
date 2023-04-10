bb33c32bb423a6b12972d63786764643c82b9aec
bykriz <cristianesquivelh@hotmail.com>
2023-04-10 18:26:50 -0400

    corrigiendo los nombres de los colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,18 @@ export default {
     colors: {
       // Primary Colors
       "slider-full-color": "#a5f3eb",
-      "slider-bg": "#10d5c2",
-      "discount-bg": "#feece7",
+      "slider": "#10d5c2",
+      "discount": "#feece7",
       "discount-text": "#ff8c66",
       "cta-text": "#bdccff",
 
       // Neutral Colors
-      "pricing-component-bg": "#ffffff",
-      "main-bg": "#fafbff",
+      "pricing-component": "#ffffff",
+      "main": "#fafbff",
       "slider-empty-color": "#eaeefb",
-      "toggle-bg": "#cdd7ee",
+      "toggle": "#cdd7ee",
       "text-color": "#858fad",
-      "cta-bg": "#293356",
+      "cta": "#293356",
     },
     extend: {},
   },
