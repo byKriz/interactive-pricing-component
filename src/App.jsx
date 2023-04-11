a71c08fbcbc47d74b4ee70a7e58603d53b7d38c2
bykriz <cristianesquivelh@hotmail.com>
2023-04-11 17:04:02 -0400

    incorpoando el pricebox

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { PriceBox } from "./components/PriceBox";
 
 function App() {
   return (
     <div className=" bg-main grid h-screen w-full place-content-center px-2 sm:px-0">
       <Header />
+      <PriceBox />
     </div>
   );
 }
