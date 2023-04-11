960e602e536f5303f39b529618c43b3eb4ba974d
bykriz <cristianesquivelh@hotmail.com>
2023-04-11 17:03:50 -0400

    agregando la logica basica del pricebox

--- src/components/PriceBox.jsx
+++ src/components/PriceBox.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+
+const equivalents = {
+  pageviews: ["10K", "50K", "100K", "500K", "1M"],
+  pirces: [8, 12, 16, 24, 36],
+};
 
 export const PriceBox = () => {
+  const [value, setValue] = useState(0);
+
   return (
-    <div>PriceBox</div>
-  )
-}
+    <main className="flex flex-col justify-center items-center mt-2 bg-pricing-component px-4">
+      <span>{equivalents.pageviews[value]} PAGEVIEWS</span>
+      <input
+        type="range"
+        min={0}
+        max={4}
+        step={1}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className=" w-full"
+      />
+      <div className="flex gap-1">
+        <p>${equivalents.pirces[value]} </p>
+        <span>/month</span>
+      </div>
+      <div className="flex">
+        <p>Monthly Billing</p>
+        <input type="text" />
+        <p>Yearly Billing</p>
+        <span>20%</span>
+      </div>
+      <div className="w-full h-[1px] bg-txt-color"></div>
+      <div>
+        <ul>
+          <li>Unlimited websites</li>
+          <li>100% data ownership</li>
+          <li>Email Reports</li>
+        </ul>
+        <a href="#" className="">
+          Star My Trial
+        </a>
+      </div>
+    </main>
+  );
+};
